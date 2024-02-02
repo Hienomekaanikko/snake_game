25fc46f664ef6b4e2b4a89d1f02721a61c573143
Mikko Suokas <121874134+Hienomekaanikko@users.noreply.github.com>
2024-02-02 17:20:56 +0200

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,11 +1,13 @@
-
 from turtle import Turtle
+
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
+
 class Snake:
 
     def __init__(self):
@@ -31,12 +33,15 @@ class Snake:
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
+
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
+
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
